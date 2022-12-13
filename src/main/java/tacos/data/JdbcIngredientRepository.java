113acebc3d35e1071530dc7b04d5409e089bb178
SafeWinter <zandong_19@aliyun.com>
2022-12-14 03:53:16 +0800

    至 S3.1.4: 实现 TacoOrder 数据持久化逻辑

--- src/main/java/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -1,6 +1,5 @@
 package tacos.data;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import tacos.Ingredient;
@@ -9,11 +8,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+
 @Repository
-public class JdbcIngredientRepository implements IngredientRepository{
+public class JdbcIngredientRepository implements IngredientRepository {
 
     // autowire jdbcTemplate via constructor
     private final JdbcTemplate jdbcTemplate;
+
     public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -27,7 +28,10 @@ public class JdbcIngredientRepository implements IngredientRepository{
 
     @Override
     public Optional<Ingredient> findById(String id) {
-        List<Ingredient> results = jdbcTemplate.query("", this::mapRowToIngredient, id);
+        List<Ingredient> results = jdbcTemplate.query(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient,
+                id);
         return results.size() == 0 ?
                 Optional.empty() :
                 Optional.of(results.get(0));
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 import javax.validation.Valid;
 
@@ -17,6 +18,12 @@ import javax.validation.Valid;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepo = orderRepository;
+    }
 
     @GetMapping("/current")
     public String orderForm() {
@@ -27,11 +34,11 @@ public class OrderController {
     public String processOrder(
             @Valid TacoOrder order, Errors errors,
             SessionStatus sessionStatus) {
-        if(errors.hasErrors()) {
+        if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";

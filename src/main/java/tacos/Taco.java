a37e0c11dfeb1788f0359929635350c0b49f41c1
SafeWinter <zandong_19@aliyun.com>
2022-12-23 17:55:36 +0800

    至 S3.2：使用 Spring Data JDBC 简化 S3.1 代码

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,6 +1,8 @@
 package tacos;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -8,10 +10,12 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+@Table
 public class Taco {
 
+    @Id
     private Long id;
-    private Date createAt = new Date();
+    private Date createdAt = new Date();
 
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
